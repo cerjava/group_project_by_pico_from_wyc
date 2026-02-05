3bb1eaec11f5dca46a05dbde3250bf54a7cf8fc4
hc_iterator <13482012512@163.com>
2026-02-05 16:50:08 +0800

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,21 @@
 int main() {
 
     stdio_init_all();
-    
 
     gpio_init(ON_BOARD_LED);
     gpio_set_dir(ON_BOARD_LED, GPIO_OUT);
     gpio_put(ON_BOARD_LED, 1);
+
+    initMotorDriver();
+
+    setPowerMotor(255, true);
+
+    gpio_init(Buzzer);
+    gpio_set_dir(Buzzer, GPIO_OUT);
+    gpio_put(Buzzer, 1);
+
+    sleep_ms(500);
+
+    //gpio_put(Buzzer, 0);
+
 };
