f04552493c5c69df4f399a58763b4e102e82a976
hc_iterator <13482012512@163.com>
2026-02-05 14:53:11 +0800

    modified:   CMakeLists.txt 	new file:   MotorControl.cpp 	modified:   RCCar.h 	renamed:    "\347\224\265\350\267\257\346\216\247\345\210\266.cpp" -> RemoteControl.cpp 	modified:   main.cpp 	deleted:    "\351\201\245\346\216\247.cpp"

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include "RCCar.h"
 
 
-
-
-
-
-
-
 int main() {
+
+    stdio_init_all();
+    
+
     gpio_init(ON_BOARD_LED);
     gpio_set_dir(ON_BOARD_LED, GPIO_OUT);
     gpio_put(ON_BOARD_LED, 1);
