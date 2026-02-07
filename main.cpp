b98af10f66ea13749600ef4e3c5224785852e42a
hc_iterator <13482012512@163.com>
2026-02-07 12:38:40 +0800

    修发射端Bug 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,34 +4,162 @@
 // 有啥用？
 const uint RF_SYNC_CODE = 0b1000101011000011;
 
-int main() {
+#include <tusb.h> // TinyUSB tud_cdc_connected()
 
-    stdio_init_all();
+int main(void)
+{
+  // initialize all present standard stdio types
+  stdio_init_all();
 
-    // 这个25你还没背出来吗？
-    gpio_init(25);
-    gpio_set_dir(25, GPIO_OUT);
-    gpio_put(25, 1);
-
-    initMotorDriver();
-
-    setPowerMotor(65535, true);
+  // wait until the CDC ACM (serial port emulation) is connected
+  while (!tud_cdc_connected()) 
+  {
+    sleep_ms(10);
+  }
 
 
-    bool left = true;
-    while(true)
+  // GPIO pin numbers
+  pin_manager_t my_pins = { 
+    .copi = 3, 
+    .cipo = 4, 
+    .sck = 2,
+    .csn = 5, 
+    .ce = 6 
+  };
+
+  /**
+   * nrf_manager_t can be passed to the nrf_client_t
+   * initialise function, to specify the NRF24L01 
+   * configuration. If NULL is passed to the initialise 
+   * function, then the default configuration will be used.
+   */
+  nrf_manager_t my_config = {
+    // AW_3_BYTES, AW_4_BYTES, AW_5_BYTES
+    .address_width = AW_5_BYTES,
+
+    // dynamic payloads: DYNPD_ENABLE, DYNPD_DISABLE
+    .dyn_payloads = DYNPD_ENABLE,
+
+    // retransmission delay: ARD_250US, ARD_500US, ARD_750US, ARD_1000US
+    .retr_delay = ARD_500US,
+
+    // retransmission count: ARC_NONE...ARC_15RT
+    .retr_count = ARC_10RT,
+
+    // data rate: RF_DR_250KBPS, RF_DR_1MBPS, RF_DR_2MBPS
+    .data_rate = RF_DR_1MBPS,
+
+    // RF_PWR_NEG_18DBM, RF_PWR_NEG_12DBM, RF_PWR_NEG_6DBM, RF_PWR_0DBM
+    .power = RF_PWR_NEG_12DBM,
+
+    // RF Channel 
+    .channel = 120
+  };
+
+  // SPI baudrate
+  uint32_t my_baudrate = 5000000;
+
+  nrf_client_t my_nrf;
+
+  // initialise my_nrf
+  nrf_driver_create_client(&my_nrf);
+
+  // configure GPIO pins and SPI
+  my_nrf.configure(&my_pins, my_baudrate);
+
+  // not using default configuration (my_nrf.initialise(NULL)) 
+  my_nrf.initialise(&my_config);
+
+  // set to Standby-I Mode
+  my_nrf.standby_mode();
+
+  // payload sent to receiver data pipe 0
+  uint8_t payload_zero = 123;
+
+  // payload sent to receiver data pipe 1
+  uint8_t payload_one[] = "Hello";
+
+  typedef struct payload_two_s { uint8_t one; uint8_t two; } payload_two_t;
+
+  // payload sent to receiver data pipe 2
+  payload_two_t payload_two = { .one = 123, .two = 213 };
+
+  // result of packet transmission
+  fn_status_t success = ERROR;
+
+  uint64_t time_sent = 0; // time packet was sent
+  uint64_t time_reply = 0; // response time after packet sent
+
+  while (1) {
+
+    // send to receiver's DATA_PIPE_0 address
+    my_nrf.tx_destination((uint8_t[]){0x37,0x37,0x37,0x37,0x37});
+
+    // time packet was sent
+    time_sent = to_us_since_boot(get_absolute_time()); // time sent
+
+    // send packet to receiver's DATA_PIPE_0 address
+    success = my_nrf.send_packet(&payload_zero, sizeof(payload_zero));
+
+    // time auto-acknowledge was received
+    time_reply = to_us_since_boot(get_absolute_time()); // response time
+
+    if (success)
     {
-        setSteeringMotor(65535, left);
-        left = !left;
-        sleep_ms(2000);
+      printf("\nPacket sent:- Response: %lluμS | Payload: %d\n", time_reply - time_sent, payload_zero);
+
+    } else {
+
+      printf("\nPacket not sent:- Receiver not available.\n");
     }
 
-    gpio_init(Buzzer);
-    gpio_set_dir(Buzzer, GPIO_OUT);
-    gpio_put(Buzzer, 1);
+    sleep_ms(3000);
 
-    sleep_ms(500);
+    // send to receiver's DATA_PIPE_1 address
+    my_nrf.tx_destination((uint8_t[]){0xC7,0xC7,0xC7,0xC7,0xC7});
 
-    //gpio_put(Buzzer, 0);
+    // time packet was sent
+    time_sent = to_us_since_boot(get_absolute_time()); // time sent
 
-};
+    // send packet to receiver's DATA_PIPE_1 address
+    success = my_nrf.send_packet(payload_one, sizeof(payload_one));
+    
+    // time auto-acknowledge was received
+    time_reply = to_us_since_boot(get_absolute_time()); // response time
+
+    if (success)
+    {
+      printf("\nPacket sent:- Response: %lluμS | Payload: %s\n", time_reply - time_sent, payload_one);
+
+    } else {
+
+      printf("\nPacket not sent:- Receiver not available.\n");
+    }
+
+    sleep_ms(3000);
+
+    // send to receiver's DATA_PIPE_2 address
+    my_nrf.tx_destination((uint8_t[]){0xC8,0xC7,0xC7,0xC7,0xC7});
+
+    // time packet was sent
+    time_sent = to_us_since_boot(get_absolute_time()); // time sent
+
+    // send packet to receiver's DATA_PIPE_2 address
+    success = my_nrf.send_packet(&payload_two, sizeof(payload_two));
+    
+    // time auto-acknowledge was received
+    time_reply = to_us_since_boot(get_absolute_time()); // response time
+
+    if (success)
+    {
+      printf("\nPacket sent:- Response: %lluμS | Payload: %d & %d\n",time_reply - time_sent, payload_two.one, payload_two.two);
+
+    } else {
+
+      printf("\nPacket not sent:- Receiver not available.\n");
+    }
+
+    sleep_ms(3000);
+  }
+  
+}
